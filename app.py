d309e774b53679e5a09409c50b174c40bec5a61e
Manuel Barrabino <manubarrabino@gmail.com>
2024-10-01 12:31:22 -0300

    log the calls

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from langgraph_bot import execute_agent
 import time
+from send_to_logger import create_sender, send_message
 
 app = Flask(__name__)
 
@@ -11,19 +12,31 @@ def status():
 
 
 @app.route("/message", methods=["POST"])
-def message():
+async def message():
     message = request.json.get("message")
     conversation_id = request.headers.get("conversationId")
     initial_time = time.time()
     result = execute_agent(message, conversation_id)
+    billable_time = time.time() - initial_time
     print(
         f"Tiempo de respuesta de Agente entero: {
-          time.time() - initial_time}"
+            billable_time}"
     )
 
     response = {"response": result["final_response"]}
+    info_for_logger = {
+        "conversationId": conversation_id,
+        "message": message,
+        "response": result["final_response"],
+        "timeStamp": time.time(),
+        "ownerId": 1,
+        "phoneNumber": "+598 099669085"
+    }
+    async with sender:
+        await send_message(sender, info_for_logger)
     return jsonify(response)
 
 
 if __name__ == "__main__":
+    sender = create_sender()
     app.run(debug=False, host="0.0.0.0", port=80)
